5549f8365cfc7e3665788c602a3fae4f83ac252b
aboulayetchakoura <aboulayet63@gmail.com>
2026-01-15 12:44:55 -0500

    return a swapped array

---
+++ 158ArrSwap.js
@@ -0,0 +1,8 @@
+function arraySwap(arr) {
+    
+  return [arr[1], arr[0]];
+}
+// Example usage:
+console.log(arraySwap([1, 2])); // should return [2, 1]
+console.log(arraySwap(["hello", "world"])); // should return ["world", "hello"]
+console.log(arraySwap([true, false])); // should return [false, true]
