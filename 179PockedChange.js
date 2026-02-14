2427be28f80b810dbe28290999d3f2f1e0e3d581
aboulayetchakoura <aboulayet63@gmail.com>
2026-02-14 13:55:18 -0500

    flip the stance when 180 deg exactly

--- 179PockedChange.js
+++ 179PockedChange.js
@@ -1,12 +1,12 @@
 function countChange(change) {
     let amount = change.reduce((sum, next)=> sum + next,0)
   
-    let dollors = Math.floor(amount/100); 
-    console.log(dollors)
+    let dolors = Math.floor(amount/100); 
+    console.log(dolors)
     let cents = amount%100;
     console.log(cents)
    
-  return `$${dollors}.${cents.toString().padStart(2,0)}`;
+  return `$${dolors}.${cents.toString().padStart(2,0)}`;
 }
 
 countChange([100, 25, 100, 1000, 5, 500, 2000, 25])
