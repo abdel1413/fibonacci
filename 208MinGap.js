b1f3df04fe84c9f60e8161193c7ed82794bc7c43
aboulayetchakoura <aboulayet63@gmail.com>
2026-03-07 12:26:21 -0500

    use object to store. index of each char as we are looping and then compare the gag btw the same char

--- 208MinGap.js
+++ 208MinGap.js
@@ -15,3 +15,30 @@ function smallestGap(str) {
 
   return result;
 }
+
+
+//time complexity O(n^2) space complexity O(n) 
+
+
+//2nd solution
+function smallestGap(str) {
+  const charIndices = {};
+  let minGap = Infinity;
+  let result = '';
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char in charIndices) {
+      const gap = i - charIndices[char] - 1;
+      if (gap < minGap) {
+        minGap = gap;
+        result = str.slice(charIndices[char] + 1, i);
+      }
+    }
+    charIndices[char] = i;
+  }
+
+  return result;
+}
+
+//time complexity O(n) space complexity O(n)    
