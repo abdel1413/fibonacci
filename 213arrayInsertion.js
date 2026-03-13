c5b5ee735f989883e0dc3b77416c88af3fb6b68a
aboulayetchakoura <aboulayet63@gmail.com>
2026-03-13 12:08:12 -0400

    use spread operator and slice method to add value into specific position

--- 213arrayInsertion.js
+++ 213arrayInsertion.js
@@ -9,4 +9,21 @@ function insertIntoArray(arr, value, index) {
   return arr;
 }
 
+//2nd solution
+function insertIntoArray(arr, value, index) {
+  if(!arr.length && !value && !index) return [0]
+  arr.splice(index, 0, value)
+  console.log(arr)
+ return arr;
+}   
+//3rd solution
+function insertValue(arr, value, index) {
+  return [
+    ...arr.slice(0, index),
+    value,
+    ...arr.slice(index)
+  ];
+}
+
+
  insertIntoArray([2, 4, 8, 10], 6, 2)
