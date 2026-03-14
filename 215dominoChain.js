7220e5d6f6fa523ebc4c47d2075810ff5ed92713
aboulayetchakoura <aboulayet63@gmail.com>
2026-03-14 12:18:53 -0400

    return true if the second element matches the first element of the next sub array

---
+++ 215dominoChain.js
@@ -0,0 +1,11 @@
+function isValidDominoChain(dominoes) {
+
+for(let i = 0 ;i< dominoes.length-1; i++){
+if( dominoes[i][1] !== dominoes[i+1][0]){
+  return false
+} 
+
+}
+return true
+}
+ isValidDominoChain([[6, 2], [3, 4], [4, 1]])
