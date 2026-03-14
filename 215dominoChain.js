ee31d26cd3a08b5ca85810d6d60290093f05fb1f
aboulayetchakoura <aboulayet63@gmail.com>
2026-03-14 12:23:17 -0400

    compare index and the length of sub array

--- 215dominoChain.js
+++ 215dominoChain.js
@@ -12,4 +12,8 @@ return true
 //2nd solution
 const isValidDominoChain = d => d.every((v,i)=>!i || d[i-1][1]===v[0]);
 
+//3rd solution
+const isValidDominoChain = d => d.every((v,i) => i === d.length-1 || v[1] === d[i+1][0]);
+
+
  isValidDominoChain([[6, 2], [3, 4], [4, 1]])
