1203579b56332d3cb808b93cbba88b63a6414e9a
aboulayetchakoura <aboulayet63@gmail.com>
2026-03-29 23:24:33 -0400

    return true if from finish time to current time is greater than 48;

---
+++ 228timeStamp.js
@@ -0,0 +1,18 @@
+function canRetake(finishTime, currentTime) {
+    //convert to date objects
+     let finish  = new Date(finishTime)
+     let curr = new Date(currentTime)
+   //calculate the difference in milliseconds
+    let diff = curr -finish;
+   
+    //convert to hours
+    //1s = 1000ms, 1m = 60s, 1h = 60m
+    const hours= diff/(1000 * 60 * 60);
+    console.log(hours)
+    return hours>=48
+
+}
+
+ canRetake("2026-03-23T08:00:00", "2026-03-25T14:00:00")
+
+ canRetake("2026-03-24T14:00:00", "2026-03-25T10:00:00")
