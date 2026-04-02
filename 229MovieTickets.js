4e0dc8149e62e692666757c62babbdbf1ee3c02f
aboulayetchakoura <aboulayet63@gmail.com>
2026-04-02 12:08:34 -0400

    2nd version

--- 229MovieTickets.js
+++ 229MovieTickets.js
@@ -26,6 +26,33 @@ function getMovieNightCost(day, showtime, numberOfTickets) {
 }
 
 
+// 2end solution 
+function  getMovieNightCost(day, showtime, numberOfTickets) {
+
+
+  // convert to 24-hour format
+  if (period === "pm" && hour !== 12) hour += 12;
+  if (period === "am" && hour === 12) hour = 0;
+
+  let isMatinee = hour < 17; // before 5pm
+
+  let price;
+
+  if (day === "Tuesday") {
+    price = 5;
+  } else if (["Friday", "Saturday", "Sunday"].includes(day)) {
+    price = 12;
+    if (isMatinee) price -= 2;
+  } else {
+    price = 10;
+    if (isMatinee) price -= 2;
+  }
+
+  let total = price * numberOfTickets;
+
+  return `$${total.toFixed(2)}`;
+  }
+
 getMovieNightCost("Saturday", "10:00pm", 1)
  getMovieNightCost("Sunday", "10:00am", 1)
 //  getMovieNightCost("Tuesday", "7:20pm", 2)
