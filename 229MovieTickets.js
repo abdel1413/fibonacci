29b0b13804300ca00d0b97968c91f4d1e7d6dcb4
aboulayetchakoura <aboulayet63@gmail.com>
2026-04-02 22:39:37 -0400

    practice

--- 229MovieTickets.js
+++ 229MovieTickets.js
@@ -1,6 +1,6 @@
 function getMovieNightCost(day, showtime, numberOfTickets) {
   let [time, period] = [showtime.slice(0, -2), showtime.slice(-2)];
-  let [hour, minute] = time.split(":").map(Number);
+  let [hour, minute] =  time.split(":").map(Number);
 
   // convert to 24-hour format
   if (period === "pm" && hour !== 12) hour += 12;
@@ -28,7 +28,7 @@ function getMovieNightCost(day, showtime, numberOfTickets) {
 
 // 2nd solution 
 function getMovieNightCost(day, showtime, numberOfTickets) {
-  
+
 //get hours 
 let hours = +showtime.split(":")[0]
 console.log(hours)  
@@ -63,3 +63,22 @@ getMovieNightCost("Saturday", "10:00pm", 1)
  getMovieNightCost("Sunday", "10:00am", 1)
 //  getMovieNightCost("Tuesday", "7:20pm", 2)
 //  getMovieNightCost("Wednesday", "5:40pm", 3)
+
+
+function practice(day, showtime , tickets){
+  let hours = +showtime.split(":")[0]
+  let isPm= showtime.includes("pm");
+  if(isPm && hours !==12)hours+=12;
+  if(!isPm && hours ===12)hours +=0;
+
+  let isMatinee =hours < 17;
+
+  let price = day ==="Tuesday"
+  ? 5
+  :["Friday", "Saturday", "Sunday"].includes(day)
+      ? 12 -(isMatinee? 2: 0)
+      :10- (isMatinee? 2: 0)
+      
+
+  return `$${(price * tickets).toFixed(2)}`;
+}
