c70656c9eb89df92b7b191e5fbf75434dc0d8ced
aboulayetchakoura <aboulayet63@gmail.com>
2026-04-15 12:00:55 -0400

    return the exact day

---
+++ 241WhatDayIsIt.js
@@ -0,0 +1,14 @@
+function getDayOfWeek(timestamp) {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  const date = new Date(timestamp);
+  return days[date.getUTCDay()];
+}
