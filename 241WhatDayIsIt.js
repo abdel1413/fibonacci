41c872e8a21fb5fbb7d75d32a969fb2f77d40b15
aboulayetchakoura <aboulayet63@gmail.com>
2026-04-15 12:02:39 -0400

    using short version

--- 241WhatDayIsIt.js
+++ 241WhatDayIsIt.js
@@ -12,3 +12,10 @@ function getDayOfWeek(timestamp) {
   const date = new Date(timestamp);
   return days[date.getUTCDay()];
 }
+
+
+//2nd version
+const getDayOfWeek = t => {
+  const d = new Date(t);
+  return ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][d.getUTCDay()];
+}   
