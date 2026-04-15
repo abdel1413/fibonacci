ef42b3c9b9467d1de4dba1984a79aeda38a3fa92
aboulayetchakoura <aboulayet63@gmail.com>
2026-04-15 12:03:57 -0400

    improve short version

--- 241WhatDayIsIt.js
+++ 241WhatDayIsIt.js
@@ -19,3 +19,8 @@ const getDayOfWeek = t => {
   const d = new Date(t);
   return ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][d.getUTCDay()];
 }   
+
+//3rd version 
+ const getDayOfWeek = t =>
+  ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+  [new Date(t).getUTCDay()];
