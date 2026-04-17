6660611f197064163d6f07dfaae3cc8506922fd4
aboulayetchakoura <aboulayet63@gmail.com>
2026-04-17 12:32:10 -0400

    short version

--- 242MiddlePolindrome.js
+++ 242MiddlePolindrome.js
@@ -25,6 +25,16 @@ function palindromeLocator(str) {
  
 }
 
+//2nd version
+const palindromeLocator = s => {
+  let a = [...s], r = [];
+  for(let i=a.length;i>=0;i--) r.push(a[i]);
+  let m = Math.floor(a.length/2);
+  return a.join("") === r.join("")
+    ? a.length % 2 ? a[m] : [a[m-1],a[m]].join("")
+    : "none";
+}
+
 palindromeLocator("racecar")
 palindromeLocator("11100111")
 palindromeLocator("freecodecamp")
