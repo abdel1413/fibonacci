1f8a478e77c626c3b4d8ce2c6c3df28f529e01db
aboulayetchakoura <aboulayet63@gmail.com>
2026-04-30 23:31:45 -0400

    2nd version with destructuring the values the swap them

--- 249SortedArrSwap.js
+++ 249SortedArrSwap.js
@@ -15,5 +15,28 @@ console.log(sorted)
   return sorted;
 }
 
+//2nd version
+function sortAndSwap(arr) {
+  let sorted = arr.sort((a, b) => a - b);
+
+  for (let i = 2; i < sorted.length; i += 3) {
+    [sorted[i - 1], sorted[i]] = [sorted[i], sorted[i - 1]];
+  }
+
+  return sorted;
+}
+
+//3rd version
+function sortAndSwap(arr) {
+  let sorted = arr.sort((a, b) => a - b);
+
+  for (let i = 2; i < sorted.length; i += 3) {
+    let temp = sorted[i - 1];
+    sorted[i - 1] = sorted[i];
+    sorted[i] = temp;
+  }
+
+  return sorted;
+}
 sortAndSwap([3, 1, 2, 4, 6, 5])
  sortAndSwap([9, 7, 5, 3, 1, 2, 4, 6, 8]) //[1, 2, 4, 3, 5, 7, 6, 8, 9]. 
