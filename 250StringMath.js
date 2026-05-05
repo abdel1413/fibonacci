73d00f6b731ddd754dc2bc4f666512860811549c
aboulayetchakoura <aboulayet63@gmail.com>
2026-05-04 23:52:22 -0400

    2nd version with same result

--- 250StringMath.js
+++ 250StringMath.js
@@ -3,6 +3,7 @@ function stringMath(str) {
 
   let result = Number(matches[0][0]);
 
+
   for (let i = 1; i < matches.length; i++) {
     let prev = matches[i - 1];
     let curr = matches[i];
@@ -22,4 +23,30 @@ function stringMath(str) {
   }
 
   return result;
+}
+
+
+
+//2nd version
+function stringMath(str) {
+  let matches = [...str.matchAll(/\d+/g)];
+
+  let result = Number(matches[0][0]);
+
+  for (let i = 1; i < matches.length; i++) {
+    let prev = matches[i - 1];
+    let curr = matches[i];
+
+    let gapLength = curr.index - (prev.index + prev[0].length);
+
+    let num = Number(curr[0]);
+
+    if (gapLength % 2 === 0) {
+      result += num;
+    } else {
+      result -= num;
+    }                       
+    }
+
+    return result;
 }
