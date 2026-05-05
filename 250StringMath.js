81fd7df4cf5588a6dacfda183dbda95f5297587f
aboulayetchakoura <aboulayet63@gmail.com>
2026-05-04 23:53:05 -0400

    3rd version

--- 250StringMath.js
+++ 250StringMath.js
@@ -50,3 +50,28 @@ function stringMath(str) {
 
     return result;
 }
+
+//3rd version
+function stringMath(str) {
+  let matches = [...str.matchAll(/\d+/g)];
+
+  let result = Number(matches[0][0]);
+
+  for (let i = 1; i < matches.length; i++) {
+    let prev = matches[i - 1];
+    let curr = matches[i];
+
+    let gapLength = curr.index - (prev.index + prev[0].length);
+
+    let num = Number(curr[0]);
+
+    result += gapLength % 2 === 0 ? num : -num;
+  }
+
+  return result;
+}
+
+stringMath("a1bc3d4e") //-6
+stringMath("12abc56def7") //-51
+stringMath("1234") //-5
+stringMath("a1b2c3d4e5f6g7h8i9j0") //-5     
