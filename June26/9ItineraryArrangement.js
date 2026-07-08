4306d5308163fc76b60c5ff08119441dfea9dff8
aboulayetchakoura <aboulayet63@gmail.com>
2026-07-07 23:22:07 -0400

    second version

--- June26/9ItineraryArrangement.js
+++ June26/9ItineraryArrangement.js
@@ -15,3 +15,33 @@ function countItineraries(stops) {
 
   return factorial(n) * validSplits;
 }
+
+//second version
+function countItineraries(stops) {
+  const n = stops.length;
+
+  function factorial(num) {
+    let result = 1;
+    for (let i = 2; i <= num; i++) {
+      result *= i;
+    }
+    return result;
+  }
+
+  let count = 0;
+
+  // For each ordering of the optional stops
+  const arrangementsOfStops = factorial(n);
+
+  for (let afterDinner = 0; afterDinner <= 1; afterDinner++) {
+    const beforeDinnerAndLunch = n - afterDinner;
+
+    // Need at least 1 stop before lunch
+    // Need at least 1 stop between lunch and dinner
+    for (let beforeLunch = 1; beforeLunch < beforeDinnerAndLunch; beforeLunch++) {
+      count += arrangementsOfStops;
+    }
+  }
+
+  return count;
+}
